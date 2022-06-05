92e5012be4f62891a004fbbaca194828d77912b3
Diego Ferreira <diego.ferreira@thoughtworks.com>
2022-06-05 17:33:57 -0300

    add axios and sending feedback loading

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,8 +1,11 @@
 import { FormEvent, useState } from "react";
 import { ArrowLeft } from "phosphor-react";
 
+import { api } from "../../../lib/api";
+
 import { FeedbackType, feedbackTypes } from "..";
 
+import { Loading } from "../../Loading";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
 
@@ -17,15 +20,24 @@ export const FeedbackContentStep = ({
   onFeedbackRestartRequested,
   onFeedbackSent,
 }: FeedbackContentStepProps) => {
-  const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  const handleSubmitFeedback = (event: FormEvent) => {
+  const handleSubmitFeedback = async (event: FormEvent) => {
     event.preventDefault();
 
-    console.log(screenshot, comment);
+    setIsSendingFeedback(true);
+
+    await api.post("/feedbacks", {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
+    setIsSendingFeedback(false);
 
     onFeedbackSent();
   };
@@ -58,6 +70,7 @@ export const FeedbackContentStep = ({
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Tell us in detail what's going on..."
           onChange={(e) => setComment(e.target.value)}
+          disabled={isSendingFeedback}
         />
 
         <footer className="flex gap-2 mt-2">
@@ -68,10 +81,10 @@ export const FeedbackContentStep = ({
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Send feedback
+            {isSendingFeedback ? <Loading /> : "Send feedback"}
           </button>
         </footer>
       </form>
